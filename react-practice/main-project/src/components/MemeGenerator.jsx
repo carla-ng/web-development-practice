91d33f0eb861fd0013ff9a9c8fcb685c8031370c
carla-ng <carlitashata@gmail.com>
2024-04-04 20:09:44 +0200

    Getting random image for meme.

--- react-practice/main-project/src/components/MemeGenerator.jsx
+++ react-practice/main-project/src/components/MemeGenerator.jsx
@@ -1,19 +1,56 @@
+import { useState } from "react"
+import memesData from "./../assets/data/memegenerator/memesData.js"
 
 function MemeGenerator() {
+
+    //const [memeImage, setMemeImage] = useState("https://i.imgflip.com/3i7p.jpg")
+
+    const [allMemeImages, setAllMemeImages] = useState(memesData)
+
+    const [meme, setMeme] = useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "https://i.imgflip.com/3i7p.jpg"
+    })
+
+    function getRandomMemeImage() {
+        const memes = allMemeImages.data.memes
+        const randomNumber = Math.floor(Math.random() * memes.length)
+
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: memes[randomNumber].url
+        }))
+    }
+
+    function onSubmit( event ) {
+        event.preventDefault();
+        console.log('Form submitted!')
+        getRandomMemeImage()
+    }
+
     return (
         <main className="memegenerator-form-container">
-            <form>
+
+            <form className="memegenerator-form" onSubmit={onSubmit}>
+
                 <div className="memegenerator-form-inputs">
                     <input
                         type="text"
+                        placeholder="Top text"
                     />
                     <input
                         type="text"
-                    />
+                        placeholder="Bottom text"
+                    />                    
                 </div>
 
-                <button>Create meme</button>
+                <button aria-label="Generate new meme image">Create meme</button>
+                
             </form>
+            
+            <img className="memegenerator-form-image" src={meme.randomImage} alt="Meme image" />
+
         </main>
     )
 }
